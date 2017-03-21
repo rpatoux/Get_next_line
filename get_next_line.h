f42a72a15b5fd055369d6a7c54c86ae9c4da15ba
Robin PATOUX <rpatoux@e2r9p17.42.fr>
2017-03-21 21:51:35 +0100

    gnl fini et valide

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpatoux <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/13 19:05:35 by rpatoux           #+#    #+#             */
+/*   Updated: 2017/01/29 16:28:59 by rpatoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 100
+
+int		get_next_line(const int fd, char **line);
+
+#endif
